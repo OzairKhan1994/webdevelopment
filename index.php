43c1d308aac23ce65308983547d3c617d8a7dc44
ozairkhan1994 <ozairk21@gmail.com>
2019-01-27 01:47:19 -0800

    Function added

--- index.php
+++ index.php
@@ -38,34 +38,81 @@
     // name = `${fname} ${lname}`;
     // console.log (name);
     // document.write (name);
-    var marks = 120; 
-     if(marks >= 33 && marks <= 39)
-     {
-         console.log ("Grade E");
+    // var marks = 120; 
+    //  if(marks >= 33 && marks <= 39)
+    //  {
+    //      console.log ("Grade E");
+    //  }
+    //  else if(marks >= 40 && marks <= 49)
+    //  {
+    //       console.log ("Grade D");
+    //  }
+    //  else if(marks >= 50 && marks <= 59)
+    //  {
+    //       console.log ("Grade C");
+    //  } 
+    //  else if(marks >= 60 && marks <= 69)
+    //  {
+    //   console.log ("Grade B");
+    //  }
+    //  else if(marks >= 70 && marks <= 79)
+    //  {
+    //   console.log ("Grade A");
+    //  }
+    //  else if(marks >= 80 && marks <= 100)
+    //  {
+    //   console.log ("Grade A+");
+    //  }
+    //  else 
+    //  {
+    //   console.log ("Invalid");
+    //  }
+    function add() {
+       var a= 10;
+       var b= 5;
+        var sum = a + b;
+       console.log("Addition ",sum); 
      }
-     else if(marks >= 40 && marks <= 49)
-     {
-          console.log ("Grade D");
+
+     function sub() {
+       var a= 10;
+       var b= 5;
+        var sum = a - b;
+       console.log("Subtract ",sum); 
      }
-     else if(marks >= 50 && marks <= 59)
-     {
-          console.log ("Grade C");
-     } 
-     else if(marks >= 60 && marks <= 69)
-     {
-      console.log ("Grade B");
+     function mul() {
+       var a= 10;
+       var b= 5;
+        var sum = a * b;
+       console.log("Multiply ",sum); 
      }
-     else if(marks >= 70 && marks <= 79)
-     {
-      console.log ("Grade A");
+     function div() {
+       var a= 10;
+       var b= 5;
+        var sum = a / b;
+       console.log("divide ",sum); 
      }
-     else if(marks >= 80 && marks <= 100)
-     {
-      console.log ("Grade A+");
-     }
-     else 
-     {
-      console.log ("Invalid");
+     var option = "add";
+     switch (option){
+        case "add":
+        add();
+        break;
+
+        case "sub":
+        sub();
+        break;
+
+        case "mul":
+        mul();
+        break;
+
+        case "div":
+        div();
+        break;
+
+        default:
+        console.log("Invalid");
+
      }
  </script>
 </body>

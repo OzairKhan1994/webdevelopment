19e0a5f4a9711f7c60fc7213372061386d6901df
OzairKhan1994 <46838490+OzairKhan1994@users.noreply.github.com>
2019-02-01 22:38:10 +0500

    contact us 1

--- contact.php
+++ contact.php
@@ -2,7 +2,31 @@
 
    require_once ('header.php');
  ?>
- 
+ <h1 class="text-center">CONTACT US</h1>
+ <p class="text-center">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within a matter of hours to help you.</p>
+ <div class="container">
+   <div class="row">
+     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+     	<label>Your name</label>
+        <input type="text" name="name" class="form-control">
+         <label>email</label>
+        <input type="text" name="email" class="form-control">
+        <label>Your message</label>
+        <textarea type="text"  name="message"  class="form-control"></textarea>
+        <br>
+        <button class="btn btn-success">Submit</button>
+     </div>
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+	      <i class="fas fa-map-marker-alt fa-2x"></i>
+	      <p> Maqbool Heights, Suite# 1, MZ Floor, Karachi City، Nipa Chorangi، Block 10 Gulshan-e-Iqbal, Karachi, Karachi City, Sindh 75300</p>
+	      <i class="fas fa-phone mt-4 fa-2x"></i>
+	      <p> 0334 2083922</p>
+	      <i class="fas fa-envelope mt-4 fa-2x"></i>
+	      <p> info@uilearning.com</p>
+	  </div>
+
+   </div>
+ </div>
  <?php
 
    require_once('footer.php');
